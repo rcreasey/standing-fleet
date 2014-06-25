56f3d5af26ffcb8e177cc7167b23f53d17ca9cbd
Chris van der Pennen <chris.vanderpennen@gmail.com>
2014-06-25 10:47:48 +0930

    Add a scaling factor constant to SystemMap.js so we can tune the density of the maps.  Once we're happy with it we can bake the scaling factor into map.json.

--- public/js/SystemMap.js
+++ public/js/SystemMap.js
@@ -1,7 +1,9 @@
 var SystemMap = {
   init: function() {
+    // Change this to globally adjust minimum node distance and system [x,y] scale
+    const SCALING_FACTOR = 0.75;
     log("Initializing System Map...");
-    Data.ui.map.append( Data.templates.map() );
+    Data.ui.map.append(Data.templates.map());
 
     var svg = d3.select("#system-map")
       .attr("width", Data.ui.map.width())
@@ -11,51 +13,59 @@ var SystemMap = {
     d3.json("/data/map.json", function(error, map) {
       var nodes = [];
       var links = [];
-      var padlocked = true;
-      var translate;
+      var systems = [];
+      var jumps = [];
 
       var rect_height = 17;
       var rect_width = 60;
-      var system, systems, jumps;
+
+      var padlocked = true;
+
+      var translate;
+      var system;
 
       var force = d3.layout.force()
         .size([Data.ui.map.width(), Data.ui.map.height()])
-        .charge(-250)
-        .linkDistance(75)
+        .charge(-250 * SCALING_FACTOR)
+        .linkDistance(75 * SCALING_FACTOR)
         .on("tick", function() {
 
-          systems.forEach(function (systemNode) {
-            jumps.forEach(function (jump) {
+          systems.forEach(function(systemNode) {
+            jumps.forEach(function(jump) {
               // Push each node away from any lines passing within r=20 units of its center.
               // This is implemented as a weak repulsive force that scales with r but not with
               // the layout's stabilisation factor.
               // This causes the repulsion to only minimally affect the overall map layout,
               // but as the layout stabilises nodes gently slip away from jump lines intersecting
               // their center.
-              if( jump.source === systemNode || jump.target === systemNode ) return;
+              if(jump.source === systemNode || jump.target === systemNode) {
+                return;
+              }
               const r = 20;
-              if(!lineCircleIntersection(jump.source, jump.target, systemNode, r)) return;
+              if(!lineCircleIntersection(jump.source, jump.target, systemNode, r)) {
+                return;
+              }
               var dx, dy, dn, k;
               var p = getSpPoint(jump.source, jump.target, systemNode);
               dx = systemNode.x - p.x;
               dy = systemNode.y - p.y;
               dn = Math.max(1000, dx * dx + dy * dy);
-              k = -100/dn;
+              k = -100 / dn;
               systemNode.x -= dx * k * 0.15;
               systemNode.y -= dy * k * 0.15;
             });
           });
 
-          link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+          link.attr("x1", function(d) {return d.source.x;})
+            .attr("y1", function(d) {return d.source.y;})
+            .attr("x2", function(d) {return d.target.x;})
+            .attr("y2", function(d) {return d.target.y;});
 
           node.attr("transform", function(d) {
-            return "translate(" + (d.x - rect_width/2) + "," + (d.y - rect_height/2) + ")";
+            return "translate(" + (d.x - rect_width / 2) + "," + (d.y - rect_height / 2) + ")";
           });
 
-          if( padlocked ) {
+          if(padlocked) {
             var scale = zoom.scale();
             translate = [(Data.ui.map.width() / 2 - system.x * scale), (Data.ui.map.height() / 2 - system.y * scale)];
             zoom.translate(translate);
@@ -70,8 +80,9 @@ var SystemMap = {
       var root = svg.append("g");
 
       function zoomHandler() {
-        if( padlocked && ( d3.event.translate[0] !== translate[0] || d3.event.translate[1] !== translate[1] ) )
+        if(padlocked && ( d3.event.translate[0] !== translate[0] || d3.event.translate[1] !== translate[1] )) {
           padlocked = false;
+        }
         root.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
       }
 
@@ -81,24 +92,24 @@ var SystemMap = {
         node = root.selectAll(".node");
 
       system = map.Systems[ Data.state.self.system_id ];
-      systems = $.map( map.Systems, function(s) {
-        if (s.regionID === system.regionID) {
-          return s
+      systems = $.map(map.Systems, function(s) {
+        if(s.regionID === system.regionID) {
+          return s;
         }
       });
 
       systems.forEach(function(system) {
         var anchor;
+        system.x *= SCALING_FACTOR;
+        system.y *= SCALING_FACTOR;
         nodes.push(system);
         nodes.push(anchor = { x: system.x, y: system.y, fixed: true });
         links.push({ source: system, target: anchor });
       });
 
-      jumps = [];
-
       map.Gates.forEach(function(gate) {
         var jump;
-        if ( map.Systems[gate.from].regionID == system.regionID && map.Systems[gate.to].regionID == system.regionID ) {
+        if(map.Systems[gate.from].regionID == system.regionID && map.Systems[gate.to].regionID == system.regionID) {
           jumps.push(jump = {source: map.Systems[gate.from], target: map.Systems[gate.to]});
           links.push(jump);
         }
@@ -108,19 +119,19 @@ var SystemMap = {
         .nodes(nodes)
         .links(links)
         .gravity(0)
-        .charge(function(d){
-          return d.fixed ? 0 : -1250;
-        })
-        .chargeDistance(200)
-        .linkDistance(function(l){
+        .charge(function(d) {return d.fixed ? 0 : -1250 * SCALING_FACTOR;})
+        .chargeDistance(200 * SCALING_FACTOR)
+        .linkDistance(function(l) {
           if(l.source.fixed || l.target.fixed) {
             return 0;
           }
           var dx = l.source.x - l.target.x, dy = l.source.y - l.target.y;
-          return Math.min(50, Math.sqrt(dx*dx + dy*dy));
+          return Math.min(50 * SCALING_FACTOR, Math.sqrt(dx * dx + dy * dy));
         })
         .linkStrength(function(l) {
-          if(l.source.fixed || l.target.fixed) return 0.1;
+          if(l.source.fixed || l.target.fixed) {
+            return 0.1;
+          }
           return 0.25;
         })
         .start();
@@ -133,9 +144,10 @@ var SystemMap = {
       var node_groups = node = node.data(systems)
         .enter().append("g")
         .attr("class", function(d) {
-          if (d.id == Data.state.self.system_id ) {
+          if(d.id == Data.state.self.system_id) {
             return "node current-system"
-          } else {
+          }
+          else {
             return "node"
           }
         });
@@ -152,19 +164,17 @@ var SystemMap = {
         .attr("alignment-baseline", "middle")
         .attr("x", rect_width / 2)
         .attr("y", 10)
-        .text(function(d) {
-          return d.name;
-        });
+        .text(function(d) {return d.name;});
     });
 
     // Given a line AB and a point C, finds point D such that CD is perpendicular
     // to AB
-    var getSpPoint = function(A,B,C){
-      var x1=A.x, y1=A.y, x2=B.x, y2=B.y, x3=C.x, y3=C.y;
-      var px = x2-x1, py = y2-y1, dAB = px*px + py*py;
+    var getSpPoint = function(A, B, C) {
+      var x1 = A.x, y1 = A.y, x2 = B.x, y2 = B.y, x3 = C.x, y3 = C.y;
+      var px = x2 - x1, py = y2 - y1, dAB = px * px + py * py;
       var u = ((x3 - x1) * px + (y3 - y1) * py) / dAB;
       var x = x1 + u * px, y = y1 + u * py;
-      return {x:x, y:y}; //this is D
+      return {x: x, y: y}; //this is D
     };
 
     // A square-root and division free line segment / circle intersection algorithm
@@ -175,17 +185,21 @@ var SystemMap = {
       b = B.y - A.y;
       c = C.x - A.x;
       d = C.y - A.y;
-      var rr = r*r;
-      var cadb = c*a + d*b;
-      var dacb = d*a - c*b;
-      var a2b2 = a*a + b*b;
+      var rr = r * r;
+      var cadb = c * a + d * b;
+      var dacb = d * a - c * b;
+      var a2b2 = a * a + b * b;
 
-      if (dacb*dacb > (rr*a2b2)) return false;
-      else if( c*c + d*d <= rr ) return true;
-      else if( (a-c)*(a-c) + (b-d)*(b-d) <= rr) return true;
+      if(dacb * dacb > (rr * a2b2)) {
+        return false;
+      }
+      else if(c * c + d * d <= rr) {
+        return true;
+      }
+      else if((a - c) * (a - c) + (b - d) * (b - d) <= rr) {
+        return true;
+      }
       return cadb >= 0 && cadb <= a2b2;
     }
-
   }
-
 };
