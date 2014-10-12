41dbcd5b2c786745d4f40cae57dd0c1adf822b98
Ryan C. Creasey <ryan@kaneda.net>
2014-10-12 12:52:07 -0700

    adding database checks to tests.

--- test/api.js
+++ test/api.js
@@ -1,18 +1,40 @@
-var should = require('should');
-var assert = require('assert');
-var request = require('supertest');
-var mongoose = require('mongoose');
-var winston = require('winston');
-var config = require('./config');
+var should = require('should')
+  , assert = require('assert')
+  , request = require('supertest')
+  , mongoose = require('mongoose-q')()
+  , winston = require('winston')
+  , config = require('./config')
+
+var Fleet = require('../lib/models/fleet')
+
+var db;
+before(function(done) {
+  if (mongoose.connection.db) {
+    db = mongoose.connection;
+    return done();
+  }
+  db = mongoose.connect(config.db.mongodb_url), done;
+});
 
 describe('API', function() {
   var url = 'http://0.0.0.0:5000/api';
-  var tarei = require('./fixtures/tarei-ju-.json')
+  var igb_headers = require('./fixtures/tarei-ju-.json');
 
   describe('Fleet', function() {
-    describe('Create', function() {
+    beforeEach(function(done) {
+      Fleet.collection.remove(done);
+    });
 
+    describe('Create', function() {
       describe('Invalid', function() {
+
+        afterEach(function(done) {
+          Fleet.find(function(err, fleets) {
+            fleets.should.be.empty;
+            done();
+          });
+        });
+
         it('should catch invalid sessions', function(done) {
           request(url)
             .post('/fleet/create')
@@ -28,7 +50,7 @@ describe('API', function() {
         it('should not create a fleet a short password', function(done) {
           request(url)
             .post('/fleet/create')
-            .set(tarei)
+            .set(igb_headers)
             .send({fleetPassword: 'oi'})
             .expect(200)
             .end(function(err,res) {
@@ -43,7 +65,7 @@ describe('API', function() {
         it('should not create a fleet a long password', function(done) {
           request(url)
             .post('/fleet/create')
-            .set(tarei)
+            .set(igb_headers)
             .send({fleetPassword: '7d6fec0cdf6d154618c40c9013f1732393548d9b'})
             .expect(200)
             .end(function(err,res) {
@@ -58,10 +80,18 @@ describe('API', function() {
       });
 
       describe('Valid', function() {
+
+        afterEach(function(done) {
+          Fleet.find(function(err, fleets) {
+            fleets.length.should.eql(1, 'should only create one fleet');
+            done();
+          })
+        });
+
         it('should create a fleet without a password', function(done) {
           request(url)
             .post('/fleet/create')
-            .set(tarei)
+            .set(igb_headers)
             .expect(200)
             .end(function(err,res) {
               if (err) return done(err);
@@ -77,7 +107,7 @@ describe('API', function() {
         it('should create a fleet with a password', function(done) {
           request(url)
             .post('/fleet/create')
-            .set(tarei)
+            .set(igb_headers)
             .send({fleetPassword: 'dongues'})
             .expect(200)
             .end(function(err,res) {
@@ -90,6 +120,28 @@ describe('API', function() {
               done();
             });
         });
+
+        // it('should not create a fleet if one is already joined', function(done) {
+        //   request(url)
+        //     .post('/fleet/create')
+        //     .set(igb_headers)
+        //     .expect(200)
+        //     .end(function(err, res) {
+        //       if (err) return done(err);
+        //       res.body.success.should.be.ok;
+        //
+        //       request(url)
+        //         .post('/fleet/create')
+        //         .set(igb_headers)
+        //         .expect(200)
+        //         .end(function(err, res) {
+        //           if (err) return done(err);
+        //           res.body.success.should.not.be.ok;
+        //           res.body.error.message.should.match(/Please leave your current fleet before creating a new one/);
+        //           done();
+        //         });
+        //     });
+        // });
 
       });
 
--- test/config.js
+++ test/config.js
@@ -1,9 +1,5 @@
 module.exports = {
   "db": {
-		"mongodb": "mongodb://127.0.0.1:27017/standing-fleet-test"
-	},
-	"logger": {
-		"api": "logs/api.log",
-		"exception": "logs/exceptions.log"
+		"mongodb_url": "mongodb://127.0.0.1:27017/standing-fleet-test"
 	}
 };
