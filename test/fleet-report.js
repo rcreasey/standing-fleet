59bf993d1174255edab2d08204ab8004f4573f7a
Ryan C. Creasey <ryan@kaneda.net>
2014-10-17 11:11:18 -0700

    ReportClear working.

--- test/fleet-report.js
+++ test/fleet-report.js
@@ -15,7 +15,7 @@ var Fleet = require('../lib/models/fleet')
   , Event = require('../lib/models/event')
   , Hostile = require('../lib/models/hostile')
 
-describe('Fleet API: Poll', function() {
+describe('Fleet API: Report', function() {
   var url = 'http://0.0.0.0:5000/api';
   var igb_headers = require('./fixtures/tarei-ju-.json');
 
@@ -82,7 +82,6 @@ describe('Fleet API: Poll', function() {
     });
 
     it('when submitting a report', function(done) {
-
       this.sess
         .post('/api/fleet/status')
         .set(igb_headers)
@@ -93,7 +92,7 @@ describe('Fleet API: Poll', function() {
             reporterId: igb_headers.EVE_CHARID,
             reporterName: igb_headers.EVE_CHARNAME,
             text: 'validate',
-            data: ['SirMolle']
+            data: ['SirMolle', igb_headers.EVE_CHARNAME]
           }
         })
         .expect(200)
@@ -139,13 +138,108 @@ describe('Fleet API: Poll', function() {
 
           res.body.should.have.property('events').with.lengthOf(1)
           res.body.events[0].should.have.property('type', 'reportHostile');
-          res.body.events[0].data[0].should.have.property('systemName', 'JU-OWQ');
-          res.body.events[0].data[0].should.have.property('systemId', '30002911');
+          res.body.events[0].data[0].should.have.property('systemName', igb_headers.EVE_SOLARSYSTEMNAME);
+          res.body.events[0].data[0].should.have.property('systemId', igb_headers.EVE_SOLARSYSTEMID);
 
           done();
         });
 
-    })
+    });
+
+  });
+
+  describe('should get reportClear events', function() {
+    before(function() { this.sess = new session(); });
+    after(function() { this.sess.destroy(); });
+
+    var fleet_key;
+
+    it('when creating a fleet', function(done) {
+      this.sess
+        .post('/api/fleet/create')
+        .set(igb_headers)
+        .expect(200, done)
+    });
+
+    it('when checking status of a fleet', function(done) {
+      this.sess
+        .get('/api/fleet/status')
+        .set(igb_headers)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.success.should.be.ok;
+          fleet_key = res.body.fleetKey;
+
+          done();
+        });
+    });
+
+    it('when submitting a report', function(done) {
+      this.sess
+        .post('/api/fleet/status')
+        .set(igb_headers)
+        .send({
+          scanData: {
+            systemId: igb_headers.EVE_SOLARSYSTEMID,
+            systemName: igb_headers.EVE_SOLARSYSTEMNAME,
+            reporterId: igb_headers.EVE_CHARID,
+            reporterName: igb_headers.EVE_CHARNAME,
+            text: 'clear',
+            data: [ igb_headers.EVE_CHARNAME ]
+          }
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.success.should.be.ok;
+
+          done();
+        });
+
+    });
+
+    it('when checking fleet status', function(done) {
+      this.sess
+        .get('/api/fleet/status')
+        .set(igb_headers)
+        .expect(200)
+        .end(function(err,res) {
+          if (err) return done(err);
+
+          res.body.success.should.be.ok;
+          res.body.should.have.property('events').with.lengthOf(6);
+          res.body.events[0].should.have.property('type', 'statusSelf');
+          res.body.events[1].should.have.property('type', 'statusFleet');
+          res.body.events[2].should.have.property('type', 'statusEvents');
+          res.body.events[3].should.have.property('type', 'statusMembers');
+          res.body.events[4].should.have.property('type', 'statusHostiles');
+          res.body.events[5].should.have.property('type', 'statusScans');
+
+          res.body.events[4].should.have.property('data').with.lengthOf(0)
+
+          done();
+        });
+    });
+
+    it('when polling a fleet', function(done) {
+      this.sess
+        .get('/api/fleet/poll/' + moment().unix())
+        .set(igb_headers)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.success.should.be.ok;
+
+          res.body.should.have.property('events').with.lengthOf(2)
+          res.body.events[1].should.have.property('type', 'reportClear');
+          res.body.events[1].data.should.have.property('systemName', igb_headers.EVE_SOLARSYSTEMNAME);
+          res.body.events[1].data.should.have.property('systemId', igb_headers.EVE_SOLARSYSTEMID);
+
+          done();
+        });
+
+    });
 
   });
 
