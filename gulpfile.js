aced0be912ff006f9b581f9e6b1555cd55d81352
Ryan C. Creasey <ryan@kaneda.net>
2015-01-04 23:47:31 -0800

    bundling faye.

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,11 @@ gulp.task('prepare', function() {
     .pipe(gulp.dest('public'))
     .pipe(gulp.dest('client'));
 
+  gulp.src('node_modules/faye/browser/faye-browser.js')
+    .pipe(concat('js/data-client.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public'));
+    
   gulp.src('app/templates/*.hbs')
     .pipe(handlebars())
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
