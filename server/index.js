c3ad1b9d9ce93ed85ad60d137ac57527ef21323c
Ryan C. Creasey <ryan@kaneda.net>
2015-01-06 22:09:13 -0800

    undoing ssl tweaks.

--- server/index.js
+++ server/index.js
@@ -39,6 +39,9 @@ app.set('map', settings.map);
 app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(checks.ssl_headers)
+app.use(checks.redirect_to_https)
 app.use(checks.static_rewrite);
 
 app.set('trust proxy', 1);
