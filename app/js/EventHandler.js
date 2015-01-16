f8a8484b6f4f190bc8bdf2136372b44b333b8361
Ryan C. Creasey <ryan@kaneda.net>
2015-01-15 21:23:37 -0800

    last minute fixes.

--- app/js/EventHandler.js
+++ app/js/EventHandler.js
@@ -301,7 +301,7 @@ var EventHandler = {
     Data.state.self.characterName = self.characterName;
     Data.state.self.characterId = self.characterId;
     Data.state.self.key = self.key;
-    Data.ui.bottomMenu_pilotKey.append(" " + Data.state.self.key);
+    Data.ui.bottomMenu_pilotKey.find('i').after(" " + Data.state.self.key);
     if (self.systemId) this.statusSelfSystem(self);
   },
 
