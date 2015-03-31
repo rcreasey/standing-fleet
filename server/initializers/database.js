068f85cfaf271c096ba8e79ac1ea4e2408c20663
Ryan C. Creasey <ryan@kaneda.net>
2015-03-30 18:33:56 -0700

    updating to use the right key.

--- server/initializers/database.js
+++ server/initializers/database.js
@@ -9,7 +9,7 @@ module.exports = function () {
     mongoose.connect(process.env.MONGODB_URL);
     if (process.env.NODE_ENV === 'development') mongoose.set('debug', true);
     cache(mongoose, 
-      {memServer: process.env.MEMCACHE_SERVERS, cache: false, 
+      {memServers: process.env.MEMCACHE_SERVERS, cache: false, 
        memOptions: {reconnect: 1000, timeout: 1000, retries: 2, failures: 2}
     });
   };
